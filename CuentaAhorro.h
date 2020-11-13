4b1507887119195d24a2142be8ba86168d91cdbe
Diego-spes <74331292+Diego-spes@users.noreply.github.com>
2020-11-12 23:16:02 -0600

    Add files via upload

--- CuentaAhorro.h
+++ CuentaAhorro.h
@@ -7,14 +7,74 @@ class CuentaAhorro{
 	    
 	public:
 	    Datospersonales{}: Nombre(""), edad(0), Correoelectronico(""), sexo(""),Residencia(""), Numerodecuenta("0000 0000 0000 0000 0000 0000"), Fondos(0),Password("******"){}; // Valores por default
-		Datospersonales(string Nom, int ed, string Res, string Corr, string sex, Numerodecuenta, string password, int Fondos): Nombre(Nom), edad(ed), Residencia(Res),Correoelectronico(Corr), sexo(sex),Numerodecuenta(num), Fondos(Fond){};
-			
-		void setNombre(string Nom);
-		void setEdad(int ed);
-		void setResidencia(string Res);
-		void setCorreo(string Corr);
-		void setSexo(string sex);
-		void setNumero(string nom);
-		void setFondos(int fond);
-		void setPassword(string pass);
+		Datospersonales(string Nom, int ed, string Res, string Corr, string sex, string Num, string password, int Fondos): Nombre(Nom), edad(ed), Residencia(Res),Correoelectronico(Corr), sexo(sex),Numerodecuenta(num), Fondos(Fond),Password(pass){};
+		//setters	
+		void set_Nombre(string Nombre);
+		void set_Edad(int edad);
+		void set_Residencia(string Residencia);
+		void set_Correo(string Correoelectronico);
+		void set_Sexo(string sexo);
+		void set_Numero(string Numerodecuenta);
+		void set_Fondos(int Fondos);
+		void set_Password(string password);
+		//Getters	
+		void get_Nombre();
+		void get_Edad();
+		void get_Residencia();
+		void get_Correo();
+		void get_Sexo();
+		void get_Numero();
+		void get_Fondos();
+		void get_Password();
 };
+
+//Setter
+void CuentaAhorro::set_Correo(string Correoelectronico){
+	Corr=Correoelectronico;
+}
+void CuentaAhorro::set_Edad(int edad){
+	edad=ed;
+}
+void CuentaAhorro::set_Nombre(string Nombre){
+	Nom=Nombre;
+}
+void CuentaAhorro::set_Residencia(string Residencia){
+	Res=Residencia;
+}
+void CuentaAhorro::set_Sexo(string sexo){
+	sex=sexo;
+}
+void CuentaAhorro::set_Fondos(int Fondos){
+	Fond=Fondos;
+}
+void CuentaAhorro::set_Password(string password){
+	pass=Password;
+}
+void CuentaAhorro::set_Numerodecuenta(string Numerodecuenta){
+	Num=Numerodecuenta;
+}
+//Getter
+string CuentaAhorro::get_Correo(){
+	return Corr
+}
+int CuentaAhorro::get_edad(){
+	return ed;
+}
+string CuentaAhorro::get_Nombre(){
+	return Nom:
+}
+string CuentaAhorro::get_Residencia(){
+	return Res;
+}
+string CuentaAhorro::get_Numerodecuenta(){
+	return Num;
+}
+string CuentaAhorro::get_Sexo(){
+	return sex;
+}
+string CuentaAhorro::get_Password(){
+	return pass;
+}
+int CuentaAhorro::get_Fondos(){
+	return Fond;
+}
--- main copia.cpp
+++ main copia.cpp
@@ -8,5 +8,8 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	CuentaAhorro al;
+	al.set_Numerodecuenta("1234 1145 7896 4567 4561");
+	
 	return 0;
 }
